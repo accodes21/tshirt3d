e3a05b7568a6f2fc4a2066fe8631107c011e3043
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-13 19:26:12 +0530

    importing packages

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {AnimatePresence, motion} from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import config from '../config/config'
+import state from '../store';
+import {download} from '../assets'
+import {downloadCanvasToImage, reader} from '../config/helpers'
+import { EditorTabs, DecalTypes, FilterTabs } from '../config/constants';
+import {fadeAnimation, slideAnimation} from '../config/motion'
+
 
 const Customizer = () => {
   return (
