b021010951792adbb2d6dd26a36f357f95513529
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-14 14:11:11 +0530

    basic changes

---
+++ client/src/components/Tab.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tab = () => {
+  return (
+    <div>Tab</div>
+  )
+}
+
+export default Tab
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ import {
 } from '../config/motion'
 
 import state from '../store'
-import { CustomButton } from '../components/CustomButton'
+import CustomButton from '../components/CustomButton'
 
 const Home = () => {
     const snap = useSnapshot(state)
@@ -45,7 +45,7 @@ const Home = () => {
                         type='filled'
                         title='Customize'
                         handleClick={()=> state.intro = false}
-                        customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+                        customStyles='w-fit font-bold text-sm'
                     />
                 </motion.div>
 
