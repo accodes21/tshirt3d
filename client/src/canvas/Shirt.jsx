8c25cdb9d25eb02feb60a54841980e21388f623a
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-16 15:41:05 +0530

    files created

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {easing} from 'maath'
+import {useSnapshot} from 'valtio'
+import { useFrame } from '@react-three/fiber'
+import { Decal, useGLTF, useTexture } from '@react-three/drei'
+import state from '../store'
+
+const Shirt = () => {
+
+  const snap = useSnapshot(state)  
+  const {nodes, materials} = useGLTF('/shirt_baked.glb')
+  const logoTexture = useTexture(snap.logoDecal)
+  const fullTexture = useTexture(snap.fullDecal)
+  
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+
+      </mesh>
+    </group>
+  )
+}
+
+export default Shirt
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import {Environment, Center} from '@react-three/drei'
 
-const Canvas = () => {
+import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import CameraRig from './CameraRig'
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+
+      {/* <CameraRig> */}
+        {/* <Backdrop/> */}
+        <Center>
+          <Shirt/>
+        </Center>
+      {/* </CameraRig> */}
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
