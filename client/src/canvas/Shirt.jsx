24c5e4f2cde650d8ee2b8c8c377699ea2fe53769
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-17 17:53:26 +0530

    styled the shirt

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -11,9 +11,15 @@ const Shirt = () => {
   const {nodes, materials} = useGLTF('/shirt_baked.glb')
   const logoTexture = useTexture(snap.logoDecal)
   const fullTexture = useTexture(snap.fullDecal)
+
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta))
   
+  const stateString = JSON.stringify(snap)
+
   return (
-    <group>
+    <group
+      key={stateString}
+    >
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
@@ -21,7 +27,26 @@ const Shirt = () => {
         material-roughness={1}
         dispose={null}
       >
+        {snap.isFullTexture && (
+          <Decal 
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
 
+        {snap.isLogoTexture && (
+          <Decal 
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            map-anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
       </mesh>
     </group>
   )
