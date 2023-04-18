8eb92e591a25222dd8a74e6248108ad2e27b146a
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-18 19:15:58 +0530

    updated canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -8,16 +8,21 @@ import CameraRig from './CameraRig'
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25}}
+      gl={{preserveDrawingBuffer: true}}
+      className='w-full max-w-full h-full transition-all ease-in'
+    >
       <ambientLight intensity={0.5}/>
       <Environment preset='city'/>
 
-      {/* <CameraRig> */}
-        {/* <Backdrop/> */}
+      <CameraRig>
+        <Backdrop/>
         <Center>
           <Shirt/>
         </Center>
-      {/* </CameraRig> */}
+      </CameraRig>
     </Canvas>
   )
 }
