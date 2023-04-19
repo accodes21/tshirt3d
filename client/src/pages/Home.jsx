2daed6bb4f5bcc1b676747c072bda03761db6269
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-19 21:32:47 +0530

    heading styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
                 <motion.div className='home-content' {...headContainerAnimation}>
                     <motion.div {...headTextAnimation}>
                         <h1 className='head-text'>
-                            LET'S <br className='xl:block hidden' /> DO IT
+                            LET'S <br className='xl:block hidden' /> <span>DO</span> IT
                         </h1>
                     </motion.div>
                     <motion.div
