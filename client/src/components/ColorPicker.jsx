00e795d6cd985522cf54a7f264041b97240b735c
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-25 19:07:20 +0530

    completed these editor tabs

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {SketchPicker} from 'react-color'
+import { useSnapshot } from 'valtio'
+import state from '../store'
 
 const ColorPicker = () => {
+
+  const snap = useSnapshot(state)
+
   return (
-    <div>ColorPicker</div>
+    <div className='absolute left-full '>
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
 }
 
--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import CustomButton from './CustomButton'
 
-const FilePicker = () => {
+const FilePicker = ({file, setFile, readFile}) => {
   return (
-    <div>FilePicker</div>
+    <div className='filepicker-container'>
+      <div className='flex flex-1 flex-col'>
+        <input 
+          id='file-upload'
+          type="file"
+          accept='image/*'
+          onChange={(e) => setFile(e.target.files[0])} 
+        />
+        <label htmlFor="file-upload" className='filepicker-label'>
+          Upload File
+        </label>
+
+        <p className='mt-2 text-gray-600 text-sm truncate'>
+          {file === '' ? 'No file selected' : file.name}
+        </p>
+
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-3'>
+        <CustomButton
+          type='outline'
+          title='Logo'
+          handleClick={() => readFile('logo')}
+        />
+
+        <CustomButton
+          type='filled'
+          title='Full'
+          handleClick={() => readFile('full')}
+        />
+
+      </div>
+    </div>
   )
 }
 
