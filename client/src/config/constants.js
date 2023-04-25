fc508751d3286e125be36f90e99f597df03fe183
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-25 19:06:44 +0530

    added download

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
-import { swatch, fileIcon, ai, logoShirt, stylishShirt } from "../assets";
+import { swatch, fileIcon, ai, logoShirt, stylishShirt, download } from "../assets";
 
 export const EditorTabs = [
   {
@@ -25,6 +25,13 @@ export const FilterTabs = [
     icon: stylishShirt,
   },
 ];
+
+export const Download = [
+  {
+    name: 'download',
+    icon: download
+  }
+]
 
 export const DecalTypes = {
   logo: {
