67203e91823b8c4ed88814e590b03606a94ed3d3
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-25 19:07:05 +0530

    outline type

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import state from '../store'
 import { useSnapshot } from 'valtio'
+import { getContrastingColor } from '../config/helpers'
 
 const CustomButton = ({type, title, customStyles, handleClick}) => {
 
@@ -9,14 +10,21 @@ const CustomButton = ({type, title, customStyles, handleClick}) => {
         if(type === 'filled'){
             return{
                 backgroundColor : snap.color,
-                color:'#fff'
+                color: getContrastingColor(snap.color)
+            }
+        }
+        else if(type === 'outline'){
+            return{
+                borderWidth: '1px',
+                borderColor: snap.color,
+                color: snap.color
             }
         }
     }
 
   return (
     <button 
-    className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+    className={`px-2 py-1.5 flex-1 rounded-md drop-shadow-lg ${customStyles}`}
     style={generateStyle(type)}
     onClick={handleClick}
     >
