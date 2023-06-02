e55c0a7e821ab071ebbfa14555d95c36bfeac6ca
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-02 21:45:50 +0530

    nodejs server for openai

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
+import dalleRoutes from './routes/dalle.routes.js'
+
 dotenv.config();
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: "50mb"}))
+
+app.use('/api/v1/dalle',dalleRoutes)
 
 app.get("/",(req,res) => {
     res.status(200).json({message:"hello world"})
