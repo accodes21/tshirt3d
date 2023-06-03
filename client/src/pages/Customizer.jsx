a7b3f075c9b92482ca719d987f4e658f0f825ea8
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-03 10:50:39 +0530

    updated URL

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -57,7 +57,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true)
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://tshirt3d-fnoy.vercel.app/api/v1/dalle', {
         method:'POST',
         headers: {
           'Content-Type': 'application/json'
