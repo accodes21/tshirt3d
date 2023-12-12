8be5b74d7a2546824ad46716cae7a3cb765ac79d
Aarya Chopkar <aaryachopkar@gmail.com>
2023-12-12 19:13:01 +0530

    added hdr preset

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -16,7 +16,7 @@ const CanvasModel = () => {
       className='w-full max-w-full h-full transition-all ease-in'
     >
       <ambientLight intensity={0.5}/>
-      <Environment preset='city'></Environment>
+      <Environment files={'/city.hdr'}></Environment>
 
       <CameraRig>
         <Backdrop/>
